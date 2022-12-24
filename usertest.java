e06ac0cdfb2b54474245f058bcf414fd04fb8df4
prameshctx <prameshctx@gmail.com>
2022-12-24 15:28:57 +0000

    second commit

--- usertest.java
+++ usertest.java
@@ -17,7 +17,7 @@ public class UserTest {
 	
 	public static final Long EXPECTED_ID = 1L;
 	public static final int EXPECTED_SIZE = 1;
-    public static final String EXPECTED_USERNAME = "Wahidkhan74";
+    public static final String EXPECTED_USERNAME = "prameshctx";
     public static final String EXPECTED_PASSWD = "Wahidkhan74";
     public static final String EXPECTED_USEREMAIL = "XXXXX@gmail.com";
     private User user;
